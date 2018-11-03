c8460abb4346df001d7bc4baa444cbc2d0a59456
LiJian <1287053830@qq.com>
2018-11-03 20:39:03 +0800

    测试是否能用手控制物体随手的挥动移动

---
+++ Assets/HTCVIVE/Test.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Test : MonoBehaviour
+{
+    public SteamVR_Skeleton_JointIndexEnum fingerJointHover = SteamVR_Skeleton_JointIndexEnum.indexTip;
+    public new GameObject gameObject;
+
+
+    public Vector3 oldPos;
+    public Vector3 newPos;
+
+    public Vector3 currentDir;
+    public Vector3 newDir;
+
+    Transform renderModel;
+
+    void Start()
+    {
+        renderModel = Player.instance.rightHand.gameObject.transform;
+        oldPos = renderModel.position;
+    }
+
+
+    void Update()
+    {
+        currentDir = transform.forward;
+        transform.position += currentDir * Time.deltaTime * 4f;
+
+        newPos = renderModel.position;
+
+        if (newPos != oldPos)
+        {
+            newDir = (newPos - oldPos).normalized;
+            oldPos = newPos;
+        }
+
+        newDir = Vector3.Lerp(currentDir, newDir, 100 * Time.deltaTime);
+
+        gameObject.transform.localRotation = Quaternion.FromToRotation(currentDir, newDir);
+
+
+
+    }
+}
