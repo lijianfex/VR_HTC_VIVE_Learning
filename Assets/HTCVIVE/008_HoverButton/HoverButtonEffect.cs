cf62e1c75e50eefce0f7b508afd4bb21ee121d9c
LiJian <1287053830@qq.com>
2018-11-06 21:22:40 +0800

    完成实体按钮交互测试与制作案例

---
+++ Assets/HTCVIVE/008_HoverButton/HoverButtonEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class HoverButtonEffect : MonoBehaviour
+{
+
+    public void OnButtonDown(Hand fromHand)
+    {
+        ColorSelf(Color.cyan);
+        fromHand.TriggerHapticPulse(1000);
+    }
+
+    public void OnButtonUp(Hand fromHand)
+    {
+        ColorSelf(Color.red);
+    }
+
+    private void ColorSelf(Color newColor)
+    {
+        Renderer[] renderers = this.GetComponentsInChildren<Renderer>();
+        for (int rendererIndex = 0; rendererIndex < renderers.Length; rendererIndex++)
+        {
+            renderers[rendererIndex].material.color = newColor;
+        }
+    }
+}
